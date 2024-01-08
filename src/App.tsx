59e405525dce4443c79a2d4c81ffa29c670a5f60
ggome1 <bookincar@naver.com>
2024-01-08 15:30:06 +0900

    :wrench:[chore]: 프로젝트 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 import './index.css'
 
 function App() {
-  return <button className="btn btn-outline">test</button>
+  return (
+    <>
+      <button className="btn btn-outline">test</button>
+      <div className="font-preblack">가나다라마바아사자차카타파하</div>
+      <div className="font-prebold">가나다라마바아사자차카타파하</div>
+      <div className="font-preextrabold">가나다라마바아사자차카타파하</div>
+      <div className="font-preextralight">가나다라마바아사자차카타파하</div>
+      <div className="font-prelight">가나다라마바아사자차카타파하</div>
+      <div className="font-premedium">가나다라마바아사자차카타파하</div>
+      <div className="font-preregular">가나다라마바아사자차카타파하</div>
+      <div className="font-presemibold">가나다라마바아사자차카타파하</div>
+      <div className="font-prethin">가나다라마바아사자차카타파하</div>
+    </>
+  )
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        preblack: ['preblack'],
+        prebold: ['prebold'],
+        preextrabold: ['preextrabold'],
+        preextralight: ['preextralight'],
+        prelight: ['prelight'],
+        premedium: ['premedium'],
+        preregular: ['preregular'],
+        presemibold: ['presemibold'],
+        prethin: ['prethin'],
+      },
+    },
   },
   plugins: [require('daisyui')],
 }
